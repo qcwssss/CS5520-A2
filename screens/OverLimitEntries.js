8551213c42c03a1339bbab3e98272c3c7561ea2a
Chen Qiu <qcwssss@outlook.com>
2023-02-25 16:59:05 -0800

    filter overlimits entries

--- screens/OverLimitEntries.js
+++ screens/OverLimitEntries.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import EntriesList from "../components/EntriesList";
-import { overlimits } from "../mock/over-limit";
+import { entries } from "../mock/entryData";
+
 import styles from "../styles/styles";
 
 const OverLimitEntries = () => {
+  const overlimits = entries.filter((entry) => entry.calorie > 600);
   return (
     <SafeAreaView style={styles.screenContainer}>
       <EntriesList data={overlimits} />
